ee8e15692f7e543cdaf9dbe921937475c03cd131
PNYWT <punyawat.suwannatat@hotmail.com>
2023-09-17 17:57:49 +0700

    update

--- Software construction/AllWeek/6.Dictionary.py
+++ Software construction/AllWeek/6.Dictionary.py
@@ -35,11 +35,48 @@
 # right_wings  = {value: key for key, value in floor3_rooms.items() if value in [4, 5]}
 # print(right_wings)
 
-scores = {'Beth': 66, 'Nancy': 79, 'Pat': 82, 'Zoe': 57, 'Ken': 94}
-scores['Pat'] = 89
-scores2 = scores.copy()
-scores3 = scores
-scores2['Beth'] = 65
-scores['Zoe'] = 63
-print(scores2)
-print(scores3)
+# scores = {'Beth': 66, 'Nancy': 79, 'Pat': 82, 'Zoe': 57, 'Ken': 94}
+# scores['Pat'] = 89
+# scores2 = scores.copy()
+# scores3 = scores
+# scores2['Beth'] = 65
+# scores['Zoe'] = 63
+# print(scores2)
+# print(scores3)
+
+# text = input("Enter text: ")
+# char_count = {}
+# for char in text:
+#     if char != ' ':
+#         char_count[char] = char_count.get(char, 0) + 1
+# print(char_count)
+# for char, count in char_count.items():
+#     print(f"There are {count} {char}'s")
+
+
+student_scores = {}
+total_below_60 = 0
+count_below_60 = 0
+while True:
+    name = input("Enter name: ")
+    if name == 'Q':
+        break
+    try:
+        score = float(input("Enter score: "))
+    except ValueError:
+        continue
+    student_scores[name] = score
+
+print(student_scores)
+
+for name, score in student_scores.items():
+    if score < 60:
+        total_below_60 += score
+        count_below_60 += 1
+    print(f"{name} got {score:.2f}")
+
+if count_below_60 > 0:
+    average_below_60 = total_below_60 / count_below_60
+    print(f"Average scores of students who got below 60 = {average_below_60:.2f}")
+else:
+    print("No one got below 60.")
---
+++ Software construction/AllWeek/7.String.py
@@ -0,0 +1,123 @@
+# def is_palindrome(s):
+#     s = s.replace(" ", "").lower()
+#     return s == s[::-1]
+# word = input("Enter a word: ")
+# if is_palindrome(word):
+#     print(f"{word} is a Palindrome")
+# else:
+#     print(f"{word} is not a Palindrome")
+
+
+# def is_valid_ipv4(ip):
+#     parts = ip.split(".")
+#     if len(parts) != 4:
+#         return False
+#     for part in parts:
+#         try:
+#             num = int(part)
+#             if num < 0 or num > 255:
+#                 return False
+#         except ValueError:
+#             return False
+
+#     return True
+
+# ip = input("")
+# if is_valid_ipv4(ip):
+#     print("Valid IP Address")
+# else:
+#     print("Invalid IP Address")
+
+
+# def binary_to_decimal(binary):
+#     decimal = 0
+#     power = 0
+#     for digit in reversed(binary):
+#         if digit == '1':
+#             decimal += 2 ** power
+#         power += 1
+
+#     return decimal
+
+# binary = input("Enter binary: ")
+# decimal = binary_to_decimal(binary)
+# print(decimal)
+
+
+# text = input("Input text: ")
+# n = int(input("N: "))
+# lines = [text[i:i+n] for i in range(0, len(text), n)]
+# for line in lines:
+#     print(line)
+
+
+# รับข้อความและจำนวนตัวอักษรต่อหลัก
+# text = input("Input text: ")
+# n = int(input("N: "))
+# chunks = [text[i:i + n] for i in range(0, len(text), n)]
+# max_len = max(len(chunk) for chunk in chunks)
+# formatted_chunks = [chunk.ljust(max_len) for chunk in chunks]
+# vertical_text = "\n".join("".join(row) for row in zip(*formatted_chunks))
+# print(vertical_text)
+
+# def is_password_strong(password):
+#     # Initialize flags for each condition
+#     has_length = len(password) >= 8
+#     has_upper = any(char.isupper() for char in password)
+#     has_lower = any(char.islower() for char in password)
+#     has_digit = any(char.isdigit() for char in password)
+#     has_special = any(char in "!@#$%^&*" for char in password)
+#     conditions = [has_length, has_upper, has_lower, has_digit, has_special]
+#     messages = [
+#         "The password length must be greater than or equal to 8",
+#         "The password must contain one or more uppercase characters",
+#         "The password must contain one or more lowercase characters",
+#         "The password must contain one or more numeric values",
+#         "The password must contain one or more special characters"
+#     ]
+#     error_messages = [messages[i] for i in range(5) if not conditions[i]]
+#     if not error_messages:
+#         return "Your password is strong!"
+#     else:
+#         return "\n".join(error_messages)
+# password = input("Enter a password: ")
+# result = is_password_strong(password)
+# print(result)
+
+
+def transcribe(dna):
+    rna = ""
+    for base in dna:
+        if base == "A":
+            rna += "U"
+        elif base == "C":
+            rna += "G"
+        elif base == "G":
+            rna += "C"
+        elif base == "T":
+            rna += "A"
+    return rna
+
+def translate(rna):
+    amino_acids = 0
+    i = 0
+    while i < len(rna):
+        if rna[i:i+3] == "AUG":  # หา start codon
+            amino_acids += 1
+            i += 3  # ข้าม start codon
+            while i < len(rna):
+                codon = rna[i:i+3]
+                if codon in ("UAA", "UGA", "UAG"):  # หา stop codon
+                    break
+                amino_acids += 1
+                i += 3
+        else:
+            i += 1
+    return amino_acids
+
+dna = input("DNA: ")
+rna = transcribe(dna)
+amino_acids = translate(rna)
+print(f"{amino_acids} Amino acid(s)")
+
+
